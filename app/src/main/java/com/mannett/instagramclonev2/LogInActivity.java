5ec77c2b9a9e773182ccc637c0d5bba13ed0f7ab
chelomannett <chelomannett@gmail.com>
2020-07-20 10:37:27 -0400

    Log In features Added Log Out features Added

--- app/src/main/java/com/mannett/instagramclonev2/LogInActivity.java
+++ app/src/main/java/com/mannett/instagramclonev2/LogInActivity.java
@@ -2,15 +2,63 @@ package com.mannett.instagramclonev2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.shashank.sony.fancytoastlib.FancyToast;
 
 public class LogInActivity extends AppCompatActivity {
 
-
+    private EditText edtEmailLogIn, edtPasswordLogIn;
+    private Button btnLogIn, btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        edtEmailLogIn = findViewById(R.id.edtEmailLogIn);
+        edtPasswordLogIn = findViewById(R.id.edtPasswordLogIn);
+        btnLogIn = findViewById(R.id.btnLogIn);
+        btnSignUp = findViewById(R.id.btnSignUp);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LogInActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnLogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                ParseUser.logInInBackground(edtEmailLogIn.getText().toString(),
+                        edtPasswordLogIn.getText().toString(), new LogInCallback() {
+                            @Override
+                            public void done(ParseUser user, ParseException e) {
+
+                                if (user != null && e == null){
+                                    FancyToast.makeText(LogInActivity.this,  user.getUsername() +" is logged in", FancyToast.LENGTH_LONG, FancyToast.SUCCESS, true).show();
+                                    Intent intent = new Intent(LogInActivity.this, WelcomeActivity.class);
+                                    startActivity(intent);
+
+                                }else{
+                                    FancyToast.makeText(LogInActivity.this, e.getMessage(), FancyToast.LENGTH_LONG, FancyToast.ERROR, true).show();
+
+                                }
+                            }
+                        });
+
+            }
+        });
+
     }
 }
--- app/src/main/java/com/mannett/instagramclonev2/WelcomeActivity.java
+++ app/src/main/java/com/mannett/instagramclonev2/WelcomeActivity.java
@@ -3,12 +3,35 @@ package com.mannett.instagramclonev2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.parse.ParseUser;
+import com.shashank.sony.fancytoastlib.FancyToast;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    private TextView txtWelcome;
+    private Button btnLogOut;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        txtWelcome = findViewById(R.id.txtWelcome);
+        btnLogOut = findViewById(R.id.btnLogOut);
+
+        txtWelcome.setText("Welcome "+ ParseUser.getCurrentUser().getUsername() +"!");
+
+        btnLogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FancyToast.makeText(WelcomeActivity.this,  ParseUser.getCurrentUser().getUsername() +" is logged out", FancyToast.LENGTH_SHORT, FancyToast.SUCCESS, true).show();
+                ParseUser.logOut();
+                finish();
+            }
+        });
     }
 }

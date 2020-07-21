c6925898a4802e637abdab5145b809de91bace95
chelomannett <chelomannett@gmail.com>
2020-07-20 23:13:48 -0400

    Added ProgressDialog in SignUp Activity Bug fixed at signing up (email, username and password are required)

--- app/src/main/java/com/mannett/instagramclonev2/SignUpActivity.java
+++ app/src/main/java/com/mannett/instagramclonev2/SignUpActivity.java
@@ -2,6 +2,7 @@ package com.mannett.instagramclonev2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -48,28 +49,44 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
 
             case R.id.btnSignUp_signUp_activity:
 
-                final ParseUser appUser = new ParseUser();
-                appUser.setUsername(edtUsernameSignUp.getText().toString());
-                appUser.setEmail(edtEmailSignUp.getText().toString());
-                appUser.setPassword(edtPasswordSignUp.getText().toString());
+                if (edtEmailSignUp.getText().toString().equals("") ||
+                        edtUsernameSignUp.getText().toString().equals("") ||
+                        edtPasswordSignUp.getText().toString().equals("")){
+                    FancyToast.makeText(SignUpActivity.this,
+                            "Email, Username and Password are required",
+                            FancyToast.LENGTH_SHORT, FancyToast.INFO, true).show();
 
-                appUser.signUpInBackground(new SignUpCallback() {
-                    @Override
-                    public void done(ParseException e) {
-                        if (e == null){
-                            FancyToast.makeText(SignUpActivity.this,
-                                    appUser.get("username")+" is signed up successfully",
-                                    FancyToast.LENGTH_LONG, FancyToast.SUCCESS, true).show();
-                            //Intent intent = new Intent(SignUpActivity.this,WelcomeActivity.class);
-                            //startActivity(intent);
-                        }else {
-                            FancyToast.makeText(SignUpActivity.this, "There was an error: "+ e.getMessage(),
-                                    FancyToast.LENGTH_LONG, FancyToast.ERROR, true).show();
+                }else {
+
+                    final ParseUser appUser = new ParseUser();
+                    appUser.setUsername(edtUsernameSignUp.getText().toString());
+                    appUser.setEmail(edtEmailSignUp.getText().toString());
+                    appUser.setPassword(edtPasswordSignUp.getText().toString());
+
+                    final ProgressDialog progressDialog = new ProgressDialog(this);
+                    progressDialog.setMessage("Signing up " + edtUsernameSignUp.getText().toString());
+                    progressDialog.show();
+
+                    appUser.signUpInBackground(new SignUpCallback() {
+                        @Override
+                        public void done(ParseException e) {
+                            if (e == null) {
+                                FancyToast.makeText(SignUpActivity.this,
+                                        appUser.get("username") + " is signed up successfully",
+                                        FancyToast.LENGTH_LONG, FancyToast.SUCCESS, true).show();
+                                //Intent intent = new Intent(SignUpActivity.this,WelcomeActivity.class);
+                                //startActivity(intent);
+                            } else {
+                                FancyToast.makeText(SignUpActivity.this, "There was an error: " + e.getMessage(),
+                                        FancyToast.LENGTH_LONG, FancyToast.ERROR, true).show();
 
 
+                            }
+                            progressDialog.dismiss();
                         }
-                    }
-                });
+                    });
+
+                }
 
             break;
             case R.id.btnLogIn_signUp_activity:
